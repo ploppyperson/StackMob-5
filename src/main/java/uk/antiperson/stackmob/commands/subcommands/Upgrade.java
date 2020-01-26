00b39e01c7de5e104f77c5267b58ba611a94cf66
Aurelien30000 <aurelien.domino@gmail.com>
2020-01-26 10:41:06 +0100

    fixup! Minor changes / proposals : - Removed unused import - Changed to parseInt in spawn subcommand (should be better) - A renaamed entity shouldn't be stacked again -> stack data deletion - 2 new traits for Bees, I think they are interesting as long as they prevent quick production of honey or a massive bee attack ^^

--- src/main/java/uk/antiperson/stackmob/commands/subcommands/Upgrade.java
+++ src/main/java/uk/antiperson/stackmob/commands/subcommands/Upgrade.java
@@ -1,6 +1,5 @@
 package uk.antiperson.stackmob.commands.subcommands;
 
-import org.bukkit.command.CommandSender;
 import uk.antiperson.stackmob.StackMob;
 import uk.antiperson.stackmob.commands.CommandMetadata;
 import uk.antiperson.stackmob.commands.SubCommand;
