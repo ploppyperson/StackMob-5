0e31b683ff8571399f0c94d46842872c9d86d631
Nathat23 <nathat890@outlook.com>
2020-04-18 00:51:09 +0100

    Use Jobs metadata instead so that exp is also not given by stacked mobs.

--- src/main/java/uk/antiperson/stackmob/hook/hooks/JobsHook.java
+++ src/main/java/uk/antiperson/stackmob/hook/hooks/JobsHook.java
@@ -1,34 +1,22 @@
 package uk.antiperson.stackmob.hook.hooks;
 
-import com.gamingmesh.jobs.api.JobsPrePaymentEvent;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import com.gamingmesh.jobs.Jobs;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.metadata.FixedMetadataValue;
 import uk.antiperson.stackmob.StackMob;
 import uk.antiperson.stackmob.hook.Hook;
 import uk.antiperson.stackmob.hook.HookMetadata;
+import uk.antiperson.stackmob.hook.SpawnHook;
 
 @HookMetadata(name = "Jobs", config = "jobs")
-public class JobsHook extends Hook implements Listener {
+public class JobsHook extends Hook implements SpawnHook {
 
-    private StackMob sm;
     public JobsHook(StackMob sm) {
         super(sm);
-        this.sm = sm;
     }
 
     @Override
-    public void onEnable() {
-        sm.getServer().getPluginManager().registerEvents(this, sm);
-    }
-
-    @EventHandler
-    public void onJobsPayment(JobsPrePaymentEvent event) {
-        if (event.getLivingEntity() == null) {
-            return;
-        }
-        if (!sm.getEntityManager().isStackedEntity(event.getLivingEntity())) {
-            return;
-        }
-        event.setCancelled(true);
+    public void onSpawn(LivingEntity spawned) {
+        spawned.setMetadata(Jobs.getPlayerManager().getMobSpawnerMetadata(), new FixedMetadataValue(getPlugin(), true));
     }
 }
