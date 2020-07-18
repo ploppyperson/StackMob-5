6457165de6e219742958cced1163d4137b5d2e6a
Nathat23 <nathat890@outlook.com>
2020-07-18 12:31:27 +0100

    Support for 1.16 hex color codes.

--- src/main/java/uk/antiperson/stackmob/utils/Utilities.java
+++ src/main/java/uk/antiperson/stackmob/utils/Utilities.java
@@ -1,6 +1,6 @@
 package uk.antiperson.stackmob.utils;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,16 +9,29 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utilities {
 
     public static final String PREFIX = ChatColor.DARK_GREEN + "StackMob " + ChatColor.GRAY + ">> " + ChatColor.RESET;
-
     public static final String SLIME_METADATA = "deathcount";
-
     public static final String DISCORD = "https://discord.gg/fz9xzuB";
     public static final String GITHUB = "https://github.com/Nathat23/StackMob-5";
     public static final String GITHUB_DEFAULT_CONFIG = GITHUB + "/tree/master/src/main/resources";
+    private static final Pattern hexPattern = Pattern.compile("&#([a-zA-Z0-9]){6}");
+
+    public static String translateColorCodes(String toTranslate) {
+        Matcher matcher = hexPattern.matcher(toTranslate);
+        while (matcher.find()) {
+            net.md_5.bungee.api.ChatColor chatColor = net.md_5.bungee.api.ChatColor.of(matcher.group().substring(1,8));
+            String before = toTranslate.substring(0, matcher.start());
+            String after = toTranslate.substring(matcher.end());
+            toTranslate = before + chatColor + after;
+            matcher = hexPattern.matcher(toTranslate);
+        }
+        return ChatColor.translateAlternateColorCodes('&', toTranslate);
+    }
 
     public static CompletableFuture<DownloadResult> downloadFile(File filePath, String url) {
         return CompletableFuture.supplyAsync(() -> {
