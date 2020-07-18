2af3d66ce9924ee2f7974ea43b321806d1f62e85
Nathat23 <nathat890@outlook.com>
2020-07-18 10:33:07 +0100

    Fix lead being dropped when stack dies while leash trait is enabled, allowing duplication (fixes #113)

---
+++ src/main/java/uk/antiperson/stackmob/listeners/LeashListener.java
@@ -0,0 +1,31 @@
+package uk.antiperson.stackmob.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDropItemEvent;
+import uk.antiperson.stackmob.StackMob;
+
+@ListenerMetadata(config = "traits.leashed")
+public class LeashListener implements Listener {
+
+    private final StackMob sm;
+    public LeashListener(StackMob sm) {
+        this.sm = sm;
+    }
+
+    @EventHandler
+    public void onLeash(EntityDropItemEvent event) {
+        if (event.getItemDrop().getItemStack().getType() != Material.LEAD) {
+            return;
+        }
+        if (!sm.getEntityManager().isStackedEntity((LivingEntity) event.getEntity())) {
+            return;
+        }
+        if (!event.getEntity().isDead()) {
+            return;
+        }
+        event.setCancelled(true);
+    }
+}
