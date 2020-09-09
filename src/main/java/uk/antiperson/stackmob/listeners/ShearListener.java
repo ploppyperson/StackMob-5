eab4c7bf7529d0b84dcfc7df3e0b8686b392b32a
LogGits <landogowner@gmail.com>
2020-09-09 15:38:46 +1000

    Null check and changed switch to if else

--- src/main/java/uk/antiperson/stackmob/listeners/ShearListener.java
+++ src/main/java/uk/antiperson/stackmob/listeners/ShearListener.java
@@ -60,47 +60,46 @@ public class ShearListener implements Listener {
             return null;
         }
         StackEntity stackEntity = sm.getEntityManager().getStackEntity(entity);
-        if (stackEntity.isSingle()) {
+        if (stackEntity == null || stackEntity.isSingle()) {
             return null;
         }
-        switch (sm.getMainConfig().getListenerMode(entity.getType(), "shear")) {
-            case SPLIT:
-                StackEntity slice = stackEntity.slice();
-                if (slice.getEntity() instanceof Sheep) {
-                    ((Sheep) slice.getEntity()).setSheared(false);
-                }
-                break;
-            case MULTIPLY:
-                Damageable damageable = (Damageable) item.getItemMeta();
-                int health = item.getType().getMaxDurability() - damageable.getDamage();
-                stackEntity.splitIfNotEnough(health);
-                int damage = health - stackEntity.getSize();
-                if (damage > 0) {
-                    damageable.setDamage(damageable.getDamage() + stackEntity.getSize());
-                    item.setItemMeta((ItemMeta) damageable);
-                } else {
-                    item = new ItemStack(Material.AIR);
-                }
-                if (entity instanceof Sheep) {
-                    Sheep sheared = (Sheep) entity;
-                    LootContext lootContext = new LootContext.Builder(sheared.getLocation()).lootedEntity(sheared).build();
-                    Collection<ItemStack> loot = sheared.getLootTable().populateLoot(ThreadLocalRandom.current(), lootContext);
-                    for(ItemStack itemStack : loot){
-                        if(itemStack.getData() instanceof Wool) {
-                            int woolAmount = (int) Math.round(stackEntity.getSize() * ThreadLocalRandom.current().nextDouble(1,2));
-                            Drops.dropItem(sheared.getLocation(), itemStack, woolAmount);
-                        }
+        ListenerMode shear = sm.getMainConfig().getListenerMode(entity.getType(), "shear");
+        if (shear == ListenerMode.SPLIT) {
+            StackEntity slice = stackEntity.slice();
+            if (slice.getEntity() instanceof Sheep) {
+                ((Sheep) slice.getEntity()).setSheared(false);
+            }
+        } else if (shear == ListenerMode.MULTIPLY) {
+            Damageable damageable = (Damageable) item.getItemMeta();
+            int health = item.getType().getMaxDurability() - damageable.getDamage();
+            stackEntity.splitIfNotEnough(health);
+            int damage = health - stackEntity.getSize();
+            if (damage > 0) {
+                damageable.setDamage(damageable.getDamage() + stackEntity.getSize());
+                item.setItemMeta((ItemMeta) damageable);
+            } else {
+                item = new ItemStack(Material.AIR);
+            }
+            if (entity instanceof Sheep) {
+                Sheep sheared = (Sheep) entity;
+                LootContext lootContext = new LootContext.Builder(sheared.getLocation()).lootedEntity(sheared).build();
+                Collection<ItemStack> loot = sheared.getLootTable().populateLoot(ThreadLocalRandom.current(), lootContext);
+                for (ItemStack itemStack : loot) {
+                    if (itemStack.getData() instanceof Wool) {
+                        int woolAmount = (int) Math.round(stackEntity.getSize() * ThreadLocalRandom.current().nextDouble(1, 2));
+                        Drops.dropItem(sheared.getLocation(), itemStack, woolAmount);
                     }
-                    return item;
                 }
-                MushroomCow mushroomCow = (MushroomCow) entity;
-                ItemStack mushrooms = new ItemStack(Material.RED_MUSHROOM,1);
-                Drops.dropItem(mushroomCow.getLocation(), mushrooms, (stackEntity.getSize() - 1) * 5);
-                // Spawn separate normal cow for the rest of the stack.
-                Entity cow = mushroomCow.getWorld().spawnEntity(mushroomCow.getLocation(), EntityType.COW);
-                StackEntity stackCow = sm.getEntityManager().getStackEntity((LivingEntity) cow);
-                stackCow.setSize(stackEntity.getSize() - 1);
                 return item;
+            }
+            MushroomCow mushroomCow = (MushroomCow) entity;
+            ItemStack mushrooms = new ItemStack(Material.RED_MUSHROOM, 1);
+            Drops.dropItem(mushroomCow.getLocation(), mushrooms, (stackEntity.getSize() - 1) * 5);
+            // Spawn separate normal cow for the rest of the stack.
+            Entity cow = mushroomCow.getWorld().spawnEntity(mushroomCow.getLocation(), EntityType.COW);
+            StackEntity stackCow = sm.getEntityManager().getStackEntity((LivingEntity) cow);
+            stackCow.setSize(stackEntity.getSize() - 1);
+            return item;
         }
         return null;
     }
