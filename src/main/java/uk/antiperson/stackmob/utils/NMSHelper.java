b0534fbc3905398f117004375254b63c36d2a617
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2020-11-03 21:55:08 +0100

    Update to 1.16.4

--- src/main/java/uk/antiperson/stackmob/utils/NMSHelper.java
+++ src/main/java/uk/antiperson/stackmob/utils/NMSHelper.java
@@ -1,17 +1,17 @@
 package uk.antiperson.stackmob.utils;
 
-import net.minecraft.server.v1_16_R2.DataWatcher;
-import net.minecraft.server.v1_16_R2.DataWatcherObject;
-import net.minecraft.server.v1_16_R2.DataWatcherRegistry;
-import net.minecraft.server.v1_16_R2.PacketPlayOutEntityMetadata;
-import org.bukkit.craftbukkit.v1_16_R2.entity.CraftEntity;
-import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
+import net.minecraft.server.v1_16_R3.DataWatcher;
+import net.minecraft.server.v1_16_R3.DataWatcherObject;
+import net.minecraft.server.v1_16_R3.DataWatcherRegistry;
+import net.minecraft.server.v1_16_R3.PacketPlayOutEntityMetadata;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-
 
 public class NMSHelper {
 
-    public static void sendPacket(Player player, org.bukkit.entity.Entity entity, boolean tagVisible) {
+    public static void sendPacket(Player player, Entity entity, boolean tagVisible) {
         CraftEntity craftEntity = (CraftEntity) entity;
         DataWatcher watcher = new DataWatcher(craftEntity.getHandle());
         watcher.register(new DataWatcherObject<>(3, DataWatcherRegistry.i), tagVisible);
