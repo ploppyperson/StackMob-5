40be19289ed8610f5577ad10f8a51271ad39aa25
Nathat23 <nathat890@outlook.com>
2021-04-03 12:34:47 +0100

    It is no longer the first day of April.

--- src/main/java/uk/antiperson/stackmob/StackMob.java
+++ src/main/java/uk/antiperson/stackmob/StackMob.java
@@ -56,21 +56,6 @@ public class StackMob extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        if (LocalDate.of(2021, 4, 1).equals(LocalDate.now())) {
-            int randInt = ThreadLocalRandom.current().nextInt(2, 6);
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < randInt; i++) {
-                builder.append("Stack");
-            }
-            builder.append("StackMob");
-            try {
-                Field field = Logger.class.getDeclaredField("name");
-                field.setAccessible(true);
-                field.set(getLogger(), builder.toString());
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
         getLogger().info("StackMob v" + getDescription().getVersion() + " by antiPerson and contributors.");
         getLogger().info("GitHub: " + Utilities.GITHUB);
         getLogger().info("Discord: " + Utilities.DISCORD);
