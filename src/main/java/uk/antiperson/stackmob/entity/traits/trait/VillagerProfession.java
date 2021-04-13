11c99e77f056714490a6a7eb3621283febef0e43
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2021-04-13 18:33:12 +0200

    Create VillagerProfession.java

---
+++ src/main/java/uk/antiperson/stackmob/entity/traits/trait/VillagerProfession.java
@@ -0,0 +1,20 @@
+package uk.antiperson.stackmob.entity.traits.trait;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Villager;
+import uk.antiperson.stackmob.entity.traits.Trait;
+import uk.antiperson.stackmob.entity.traits.TraitMetadata;
+
+@TraitMetadata(entity = Villager.class, path = "villager-profession")
+public class VillagerProfession implements Trait {
+
+    @Override
+    public boolean checkTrait(LivingEntity first, LivingEntity nearby) {
+        return ((Villager) first).getProfession() != ((Villager) nearby).getProfession();
+    }
+
+    @Override
+    public void applyTrait(LivingEntity spawned, LivingEntity dead) {
+        ((Villager) spawned).setProfession(((Villager) dead).getProfession());
+    }
+}
