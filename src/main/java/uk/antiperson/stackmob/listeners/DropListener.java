4c234d50569be68d74dfb16245c31406aaa11915
Nathat23 <nathat890@outlook.com>
2022-04-16 22:05:21 +0100

    Fix compile error

--- src/main/java/uk/antiperson/stackmob/listeners/DropListener.java
+++ src/main/java/uk/antiperson/stackmob/listeners/DropListener.java
@@ -3,6 +3,7 @@ package uk.antiperson.stackmob.listeners;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDropItemEvent;
@@ -23,7 +24,7 @@ public class DropListener implements Listener {
 
     @EventHandler
     public void onDropListener(EntityDropItemEvent event) {
-        if (event.getEntity() != EntityType.VILLAGER && 
+        if (!(event.getEntity() instanceof Villager) &&
                 event.getItemDrop().getItemStack().getType() != Material.EGG &&
                 event.getItemDrop().getItemStack().getType() != Material.SCUTE) {
             return;
