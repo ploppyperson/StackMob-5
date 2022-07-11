8f17c1067a3134ef8ffc41b74ced820133fdbf1c
Nathat23 <nathat890@outlook.com>
2022-07-11 12:36:18 +0100

    Add ability to spawn multiple stacks

--- src/main/java/uk/antiperson/stackmob/commands/subcommands/SpawnStack.java
+++ src/main/java/uk/antiperson/stackmob/commands/subcommands/SpawnStack.java
@@ -7,13 +7,20 @@ import uk.antiperson.stackmob.StackMob;
 import uk.antiperson.stackmob.commands.*;
 import uk.antiperson.stackmob.entity.StackEntity;
 
+import java.util.HashSet;
+import java.util.UUID;
+
 @CommandMetadata(command = "spawn", playerReq = true, desc = "Spawn a new stack.")
 public class SpawnStack extends SubCommand {
 
     private final StackMob sm;
+    private final HashSet<UUID> confirm;
     public SpawnStack(StackMob sm) {
-        super(CommandArgument.construct(ArgumentType.ENTITY_TYPE, false), CommandArgument.construct(ArgumentType.INTEGER, false, "stack size"));
+        super(CommandArgument.construct(ArgumentType.ENTITY_TYPE, false),
+                CommandArgument.construct(ArgumentType.INTEGER, false, "stack size"),
+                CommandArgument.construct(ArgumentType.INTEGER, true, "number of stacks"));
         this.sm = sm;
+        this.confirm = new HashSet<>();
     }
 
     @Override
@@ -25,10 +32,25 @@ public class SpawnStack extends SubCommand {
             sender.sendError("New stack value is too large! (max = " + sm.getMainConfig().getMaxStack(entityType) + ")");
             return false;
         }
-        LivingEntity entity = (LivingEntity) player.getWorld().spawnEntity(player.getLocation(), entityType);
-        StackEntity stackEntity = sm.getEntityManager().registerStackedEntity(entity);
-        stackEntity.setSize(newSize);
-        sender.sendSuccess("A new stack has been spawned.");
+        int amountOfStacks = args.length > 2 ? Integer.parseInt(args[2]) : 1;
+        if (amountOfStacks < 1) {
+            sender.sendError("You cannot spawn less than one stack!");
+            return false;
+        }
+        if (amountOfStacks > 20 && !confirm.contains(((Player) sender.getSender()).getUniqueId())) {
+            sender.sendInfo("Are you sure you want to spawn " + amountOfStacks + " stacks?");
+            sender.sendInfo("Run the same command again to confirm.");
+            confirm.add(((Player) sender.getSender()).getUniqueId());
+            return false;
+        }
+        for (int i = 0; i < amountOfStacks; i++) {
+            LivingEntity entity = (LivingEntity) player.getWorld().spawnEntity(player.getLocation(), entityType);
+            StackEntity stackEntity = sm.getEntityManager().registerStackedEntity(entity);
+            stackEntity.setSize(newSize);
+        }
+        String stackString = amountOfStacks == 1 ? "A new stack has" : amountOfStacks + " stacks have";
+        sender.sendSuccess(stackString + " been spawned.");
+        confirm.remove(((Player) sender.getSender()).getUniqueId());
         return false;
     }
 }
