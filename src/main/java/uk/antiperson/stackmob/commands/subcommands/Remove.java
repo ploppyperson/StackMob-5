432352f59c8a99333f9bc9976b69cead08383466
Nathat23 <nathat890@outlook.com>
2022-08-30 10:17:40 +0100

    More remove command functionality

--- src/main/java/uk/antiperson/stackmob/commands/subcommands/Remove.java
+++ src/main/java/uk/antiperson/stackmob/commands/subcommands/Remove.java
@@ -1,32 +1,39 @@
 package uk.antiperson.stackmob.commands.subcommands;
 
+import org.apache.commons.lang3.text.WordUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
 import uk.antiperson.stackmob.StackMob;
 import uk.antiperson.stackmob.commands.*;
 import uk.antiperson.stackmob.entity.StackEntity;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
-@CommandMetadata(command = "remove", playerReq = false, desc = "Remove all entities")
+@CommandMetadata(command = "remove", playerReq = false, desc = "Remove entities")
 public class Remove extends SubCommand {
 
     private final StackMob sm;
     public Remove(StackMob sm) {
-        super(CommandArgument.construct(ArgumentType.STRING, true, Arrays.asList("animals", "hostile")));
+        super(CommandArgument.construct(ArgumentType.STRING, false, Arrays.asList("chunk", "world", "all")),
+                CommandArgument.construct(ArgumentType.STRING, true, Arrays.asList("animals", "hostile")));
         this.sm = sm;
     }
 
     @Override
     public boolean onCommand(User sender, String[] args) {
         Function<Entity, Boolean> function = entity -> entity instanceof Mob;
-        if (args.length == 1) {
-            switch (args[0]) {
+        if (args.length == 2) {
+            switch (args[1]) {
                 case "animals":
                     function = entity -> entity instanceof Animals;
                     break;
@@ -35,16 +42,39 @@ public class Remove extends SubCommand {
                     break;
             }
         }
-        Set<StackEntity> toRemove = new HashSet<>();
-        for (StackEntity stackEntity : sm.getEntityManager().getStackEntities()) {
-            if (!function.apply(stackEntity.getEntity())) {
-                continue;
-            }
-            stackEntity.remove(false);
-            toRemove.add(stackEntity);
+        Set<Chunk> chunks = new HashSet<>();
+        switch (args[0]) {
+            case "chunk":
+                if (!(sender.getSender() instanceof Player)) {
+                    sender.sendError("You need to be a player!");
+                    return false;
+                }
+                chunks.add(((Player) sender.getSender()).getLocation().getChunk());
+                break;
+            case "world":
+                if (!(sender.getSender() instanceof Player)) {
+                    sender.sendError("You need to be a player!");
+                    return false;
+                }
+                chunks.addAll(List.of(((Player) sender.getSender()).getWorld().getLoadedChunks()));
+                break;
+            case "all":
+                Bukkit.getWorlds().forEach(world -> chunks.addAll(List.of(world.getLoadedChunks())));
+                break;
         }
-        toRemove.forEach(stackEntity -> sm.getEntityManager().unregisterStackedEntity(stackEntity));
-        sender.sendSuccess("Entities matching your criteria have been removed.");
+        for (Chunk chunk : chunks) {
+            for (Entity entity : chunk.getEntities()) {
+                if (!function.apply(entity)) {
+                    continue;
+                }
+                StackEntity stackEntity = sm.getEntityManager().getStackEntity((LivingEntity) entity);
+                if (stackEntity == null) {
+                    continue;
+                }
+                stackEntity.remove();
+            }
+        }
+        sender.sendSuccess(WordUtils.capitalizeFully(args[0]) + " entities matching your criteria have been removed.");
         return false;
     }
 
