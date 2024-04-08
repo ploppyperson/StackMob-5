714e9988a5099ad76d2628aa54f6b431b0892095
Nathat23 <nathat890@outlook.com>
2024-04-08 21:03:45 +0100

    Add option to disable knockback for entities

---
+++ src/main/java/uk/antiperson/stackmob/listeners/KnockbackListener.java
@@ -0,0 +1,36 @@
+package uk.antiperson.stackmob.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityKnockbackEvent;
+import uk.antiperson.stackmob.StackMob;
+import uk.antiperson.stackmob.entity.StackEntity;
+import uk.antiperson.stackmob.utils.Utilities;
+
+@ListenerMetadata(config = "disable-knockback.enabled")
+public class KnockbackListener implements Listener {
+
+    private final StackMob sm;
+    public KnockbackListener(StackMob sm) {
+        this.sm = sm;
+    }
+
+    @EventHandler
+    public void onEntityKnockback(EntityKnockbackEvent event) {
+        StackEntity stackEntity = sm.getEntityManager().getStackEntity(event.getEntity());
+        if (stackEntity == null) {
+            return;
+        }
+        if (!stackEntity.getEntityConfig().isKnockbackDisabledTypes()) {
+            return;
+        }
+        if (!(Utilities.isPaper() && stackEntity.getEntityConfig().isKnockbackDisabledReasons(event.getEntity().getEntitySpawnReason()))) {
+            return;
+        }
+        if (!stackEntity.getEntityConfig().isKnockbackDisabledCause(event.getCause())) {
+            return;
+        }
+        event.setCancelled(true);
+    }
+
+}
