811515109eff996edfb7e185bbe80772d110a4ce
ploppyperson <nathat890@outlook.com>
2026-06-29 16:33:37 +0100

    Remove warning on 26.2 (#320)

--- src/main/java/uk/antiperson/stackmob/utils/Utilities.java
+++ src/main/java/uk/antiperson/stackmob/utils/Utilities.java
@@ -203,7 +203,8 @@ public class Utilities {
         V1_20_4("1.20.4"),
         V1_20_6("1.20.6"),
         V1_21("1.21"),
-        MC26_1("26.1");
+        MC26_1("26.1"),
+        MC26_2("26.2");
 
         final String internalName;
 
